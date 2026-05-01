ba831e04b49498cc264424972b01e2702243e1ac
moazzemhossain1 <moazzemhossain202025@gmail.com>
2026-05-01 10:51:30 +0800

    The data is shwn n cards in the interface.

--- src/app/page.js
+++ src/app/page.js
@@ -14,9 +14,11 @@ const Home= async()=> {
   console.log("products",products)
 
   return (
-    <div className="">
-      oure product
-      <div>
+    <div className="container mx-auto">
+      <h3 className="text-center text-4xl font-extrabold bg-gradient-to-r from-cyan-400 via-blue-500 to-purple-500 bg-clip-text text-transparent font-bold">Our product collection</h3>
+      <p className="mt-3 mb-3 text-2xl font-medium text-center">We are sell stylish,high-quality ties. Perfect for every cccasion.</p>
+      
+      <div className="grid grid-cols-3 gap-4">
         {
           products.map((prodect,index) => <ProductCard key={index} prodect={prodect}></ProductCard>)
         }
--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,18 +1,32 @@
 import Image from 'next/image';
 import React from 'react';
+import logo1 from "@/assets/tiese.jpg"
+import Link from 'next/link';
 
 const ProductCard = ({prodect}) => {
     const {title,description,image,category,price,currency,material,dimensions,inStock}=prodect;
 
     
     return (
-        <div className='container mx-auto bg-gray-200'>
-            <div className='w-[600] rounded-xl'>
-                <div>
-                   <Image></Image>
-                </div>
+        <div className=' '>
+            <div className='bg-gradient-to-r from-[#00c6ff] via-[#0072ff] to-[#6a11cb] rounded-xl '>
+               <div className='flex justify-center items-center'>
+                 <Image className='p-3 rounded-2xl' src={logo1} alt='card image' width={400} height={400}></Image>
+               </div>
+               <div className='space-y-3 pl-3 mt-4'>
+                <h3 className='font-bold text-white text-xl'>Title:{title}</h3>
+                <h3 className='text-white  font-medium text-xl'>{category}</h3>
+              
+                <p>{}</p>
+               </div>
+               {/* {`/news/${news._id}`} */}
+               <div className='flex justify-between items-center p-3 mb-6'>
+                <p className='text-3xl font-medium text-white'>${price}</p>
+                <Link href={"/singletiles"} className="btn">Vie detieles</Link>
+               </div>
 
             </div>
+    
             
         </div>
     );
